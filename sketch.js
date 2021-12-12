b838285808a63dadbeaf3c7a86e20bcd2b4a03c5
Pragya123-cyber <80159674+Pragya123-cyber@users.noreply.github.com>
2021-12-12 17:21:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var hypnoticBall, dataBase;
+var position;
+
+
+function setup(){
+   dataBase = firebase.database()
+  createCanvas(500,500);
+
+  hypnoticBall = createSprite(250,250,10,10);
+  hypnoticBall.shapeColor = "red";
+
+var hypnoticBallposition = dataBase.ref('ball/position')  
+hypnoticBallposition.on("value",readPosition,showError);
+}
+
+function draw(){
+  background("white");
+  if(position !== undefined)
+  {
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    
+    drawSprites();
+  }
+}
+
+function writePosition(x,y){
+  dataBase.ref('ball/position').set({
+    'x': position.x+x , 
+    'y': position.y+y
+  })
+  
+}
+
+function readPosition(data) {
+position = data.val();
+hypnoticBall.x = position.x
+hypnoticBall.y = position.y
+}
+
+function showError(){
+console.log("error");
+}
